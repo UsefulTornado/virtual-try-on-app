4ef879694a04b48cd9d1fe73750a3a584fd3e493
alina8031 <al_chibizova@mail.ru>
2023-10-10 23:36:27 +0300

    макет приложения

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -11,43 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Virtual try on app',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Virtual try on app Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +31,142 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _uploadPhoto() { // поменять функцию
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      http.get(Uri.parse('http://iocontrol.ru/api/sendData/alina/bulb_1/1')).then((response) {
+        print("Response status: ${response.statusCode}");
+        print("Response body: ${response.body}");
+      }).catchError((error){
+        print("Error: $error");
+      });
     });
   }
 
+  void _tryImg(int index) { // поменять функцию
+    setState(() {
+      print(index);
+      http.get(Uri.parse('http://iocontrol.ru/api/sendData/alina/bulb_1/1')).then((response) {
+        print("Response status: ${response.statusCode}");
+        print("Response body: ${response.body}");
+      }).catchError((error){
+        print("Error: $error");
+      });
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        toolbarHeight: MediaQuery.of(context).size.height * 3/30,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          // decoration: BoxDecoration(
+          //   border: Border.all(
+          //     color: Colors.black,
+          //     width: 2,
+          //   ),
+          // ),
+          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 30, vertical: MediaQuery.of(context).size.height / 60),
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 14/15,
+                height: MediaQuery.of(context).size.height * 29/60,
+                margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 60),
+                // child: Image.asset("assets/images/hoodie.jpg", fit:BoxFit.scaleDown),
+                decoration: BoxDecoration( //здесь будет фото человека
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2,
+                  ),
+                ),
+                child: const Text('Photo'),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 60),
+                height: MediaQuery.of(context).size.height / 30,
+                // decoration: BoxDecoration(
+                //   border: Border.all(
+                //     color: Colors.red,
+                //     width: 2,
+                //   ),
+                // ),
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: _uploadPhoto,
+                  child: const Text('Upload Photo'),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 14/15,
+                height: MediaQuery.of(context).size.height * 15/60,
+                // alignment: Alignment.center,
+                margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 60),
+                // decoration: BoxDecoration(
+                //   border: Border.all(
+                //     color: Colors.black,
+                //     width: 2,
+                //   ),
+                // ),
+                child: ListView.builder(
+                  // padding: EdgeInsets.only(right: MediaQuery.of(context).size.width / 30, bottom: MediaQuery.of(context).size.height / 60),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 11/45,
+                          height: MediaQuery.of(context).size.height * 6/30,
+                          // decoration: BoxDecoration(
+                          //   border: Border.all(
+                          //     color: Colors.red,
+                          //     width: 2,
+                          //   ),
+                          // ),
+                          // child: Image.asset("assets/images/img$index.jpg", fit:BoxFit.scaleDown),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * 11/45,
+                                height: MediaQuery.of(context).size.height * 4/30,
+                                // decoration: BoxDecoration(
+                                //   border: Border.all(
+                                //     color: Colors.green,
+                                //     width: 2,
+                                //   ),
+                                // ),
+                                child: Image.asset("assets/images/img$index.jpg", fit:BoxFit.scaleDown),
+                              ),
+                              TextButton(
+                                style: ButtonStyle(
+                                  foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                                ),
+                                onPressed:(){
+                                  _tryImg(index);
+                                },
+                                child: const Text('Try'),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: MediaQuery.of(context).size.width * 3/45), // Расстояние между контейнерами
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
